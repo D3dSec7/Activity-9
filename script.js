2a730f39726a6a4edcd30b8023ebb0e6c5dd294b
D3dSec7 <nottronin@gmail.com>
2024-11-20 22:48:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function blue(){
+    document.body.style.backgroundColor = "lightblue"
+    document.body.style.fontFamily = "Oswald"
+    document.body.style.fontSize = "80px"
+}
+function pink(){
+    document.body.style.backgroundColor = "lightpink"
+    document.body.style.fontFamily = "Cedarville Cursive"
+    document.body.style.fontSize = "80px"
+}
+function brown(){
+    document.body.style.backgroundColor = "rgb(228, 165, 82)"
+    document.body.style.fontFamily = "Playfair Display"
+    document.body.style.fontSize = "80px"
+}
